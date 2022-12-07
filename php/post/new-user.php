6041a0a6dade28482908e25da0f736abdd4707d7
sophiesundqvist <sophie.sundqvist@hotmail.com>
2022-12-07 21:38:14 +0100

    created post-new.user and ran some test to dataset.json

---
+++ php/post/new-user.php
@@ -0,0 +1,63 @@
+<?php 
+require_once "../functions/functions.php";
+require_once "post.php";
+
+if(!isset($receivedData["username"], $receivedData["firstName"], $receivedData["lastName"], $receivedData["password"])){
+    $error = ["error" => "invalid credentials"];
+    sendJSON($error, 400);
+}
+
+if(strlen($receivedData["username"]) < 4){
+    $error = ["error" => "username need to have at least 4 characters"];
+    sendJSON($error, 400);
+}
+
+if(strlen($receivedData["firstName"]) < 3 or strlen($receivedData["lastName"]) < 3){
+    $error = ["error" => "firstname and lastname need to have at least 2 characters"];
+    sendJSON($error, 400);
+}
+
+if(strlen($receivedData["password"]) < 6){
+    $error = ["error" => "password need to have at least 6 characters"];
+    sendJSON($error, 400);
+}
+
+foreach($data["users"] as $user){
+    if($user["username"] == $receivedData["username"]){
+        $error = ["error" => "username already exists"];
+        sendJSON($error, 422);
+    }
+}
+ 
+// detta userID ska ändras hur de räknas ut
+$lastIndex = count($data["users"]) - 1;
+$lastUserId = $data["users"][$lastIndex]["userID"];
+
+$userId = $lastUserId + 1;
+$username = $receivedData["username"];
+$firstName = $receivedData["firstName"];
+$lastName = $receivedData["lastName"];
+$password = $receivedData["password"];
+
+$newUser = [
+    "userID" => $userId,
+    "username" => $username,
+    "firstName" => $firstName,
+    "lastName" => $lastName,
+    "password" => $password
+];
+
+$data["users"][] = $newUser;
+
+$jsonData = json_encode($data, JSON_PRETTY_PRINT);
+file_put_contents($filename, $jsonData);
+
+sendJSON($newUser);
+
+
+
+
+
+
+
+?>
