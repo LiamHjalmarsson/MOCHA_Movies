bd679f1f3073004a5fa3020b841182811fe77ed3
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2022-12-12 20:51:04 +0100

    rendermovies, createMovie
    Two options, commented code optional to work but creates new container each time filtering.. Now it fills on the same container.
    
    POPULAR function is to be removed only to see so it works

---
+++ js/showmovies/showmovies.js
@@ -0,0 +1,69 @@
+// remove this just for try 
+
+const key = `e666c096bb904490508ada0b495d2d90`; 
+
+async function popular () {
+    let popularResponse = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`);    
+    let popularResource = await popularResponse.json();
+
+    await renderMoives(popularResource, 1);
+}
+
+//************************/  
+
+async function renderMoives (movies, counter) {
+    
+    document.querySelector("main").classList.add("moivesMain");
+    
+    let movieGridContainer = document.createElement("div");
+    movieGridContainer.id = "movieGridContainer";
+
+    // movies.results.forEach(movie => {
+    //     movieGridContainer.append(createMovie(movie));
+    // });
+
+    getMovies(movies, counter, movieGridContainer);
+
+    let btn = document.createElement("button");
+    btn.classList.add("showMore");
+    btn.textContent = "show more"
+    
+    btn.addEventListener("click", async () => {
+        counter++;
+
+        // document.querySelectorAll("main > button").forEach(btn => {
+        //     btn.remove();
+        // });
+
+        let popularResponse = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=${counter}`);    
+        let popularResource = await popularResponse.json();
+
+        // renderMoives(popularResource, counter);
+        getMovies(popularResource, counter, movieGridContainer);
+    });
+
+    document.querySelector("main").append(movieGridContainer, btn);
+
+}
+
+function getMovies (movies, counter, movieGridContainer) {
+
+    movies.results.forEach(movie => {
+        movieGridContainer.append(createMovie(movie));
+    });
+
+}
+
+function createMovie (movie) {
+    let movieCard = document.createElement("div");
+    movieCard.classList.add("movieCard");
+    movieCard.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.poster_path})`;
+    
+    movieCard.addEventListener("click", () => {
+        // renderMovie 
+    });
+
+    return movieCard;
+}
+
+popular();
