ac3afa3541b49d886443b53f47660a0ebf5a42ea
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-12-23 13:14:45 +0100

    Grade system functioning
    Stars that get more full and a number grade that is available

--- js/user/index.php
+++ js/user/index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="style_stars.css">
     <title>Document</title>
 </head>
 <body>
