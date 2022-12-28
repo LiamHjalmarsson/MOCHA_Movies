a9f2c127e1fa75bf87e7e67813810f7c92104be5
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-12-28 16:35:33 +0100

    Bug fixes in user and user-movies
    added import to correct files and functions

--- js/user/index.php
+++ js/user/index.php
@@ -2,8 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="style_stars.css">
     <title>Document</title>
@@ -12,7 +10,7 @@
     <nav></nav>
     <main></main>
     <footer></footer>
-    <script type="module" src="../header/header.js"></script>
-    <script type="text/javascript" src="user.js"></script>
+
+    <script type="module" src="user.js"></script>;
 </body>
 </html>
