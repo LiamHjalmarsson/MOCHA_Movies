c0d162dee39cb1b22562e1126dc525a9326dd15d
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2022-12-29 18:49:30 +0100

    some styling

--- js/navigationBack/navigationBack.js
+++ js/navigationBack/navigationBack.js
@@ -8,11 +8,12 @@ export function navigationBack (remove, path) {
     navigationBack.id = "navigationBack";
 
     let arrowBack = document.createElement("div")
-    arrowBack.innerHTML=`<span class="material-symbols-outlined">chevron_left</span>`;
+    arrowBack.innerHTML=`<span class="material-symbols-outlined backArrow">chevron_left</span>`;
     // lagt till namn / filmens title i naven då man inte ser namnet på vissa filmer, antingen låtter vi detta ligga här  
     // eller så placerar vi det någon annans stans i filmens information 
     let name = document.createElement("div");
-    name.innerHTML = `${path}`;
+    let title = path ? path: `MochaMovies`;
+    name.innerHTML = `${title}`;
     let profile = createProfile(user)
     navigationBack.append(arrowBack, name, profile)
 
