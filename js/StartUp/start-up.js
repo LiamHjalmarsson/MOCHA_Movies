f22900bff46cb56ed189b8b39931fd8594c0d83c
Tanja Björklind <tbjorklind@gmail.com>
2023-01-05 20:58:12 +0100

    styling log In and sign Up

--- js/StartUp/start-up.js
+++ js/StartUp/start-up.js
@@ -5,12 +5,22 @@ export function logIn () {
   let main = document.querySelector('main')
   main.innerHTML = ''
 
+  let titleHeader = document.createElement('div')
+  titleHeader.classList.add("titleHeader")
+  titleHeader.innerHTML = "<h3>MochaMovies</h3>"
+
+  let logSignInTitle = document.createElement('div')
+  logSignInTitle.classList.add("logSignInTitle")
+  logSignInTitle.innerHTML = "<h2>Log In</h2>"
+
   let logInDiv = document.createElement('div')
   let signUpDiv = document.createElement('div')
   let signUpText = document.createElement('div')
   let submitButton = document.createElement('button')
   let errorDiv = document.createElement('div')
-
+  errorDiv.classList.add("errorDiv")
+  
+  logInDiv.append(titleHeader, logSignInTitle)
   logInDiv.classList.add('logIn')
 
   let array = ['Username', 'Password']
@@ -33,11 +43,13 @@ export function logIn () {
     logInDiv.append(inputDiv)
   }
 
-  submitButton.textContent = 'Log in'
+  submitButton.textContent = 'Log In'
   signUpDiv.append("if you don't have an account -")
   signUpText.textContent = 'Sign Up'
   signUpText.addEventListener('click', signUp)
-  signUpText.style.color = 'teal'
+  signUpText.style.color = '#1e608f'
+  signUpText.style.fontWeight = "bold";
+
   signUpText.style.cursor = 'pointer'
 
   signUpDiv.append(signUpText)
@@ -80,12 +92,23 @@ function signUp () {
   let main = document.querySelector('main')
   main.innerHTML = ''
 
+  let titleHeader = document.createElement('div')
+  titleHeader.classList.add("titleHeader")
+  titleHeader.innerHTML = "<h3>MochaMovies</h3>"
+
+  let logSignInTitle = document.createElement('div')
+  logSignInTitle.classList.add("logSignInTitle")
+  logSignInTitle.innerHTML = "<h2>Sign Up</h2>"
+  
   let signUpDiv = document.createElement('div')
   let logInDiv = document.createElement('div')
   let submitButton = document.createElement('button')
   let logInText = document.createElement('div')
   let errorDiv = document.createElement('div')
-
+  errorDiv.classList.add("errorDiv")
+  
+  signUpDiv.append(titleHeader, logSignInTitle)
+  
   let array = ['Username', 'Password', 'FirstName', 'LastName']
 
   for (let name of array) {
@@ -95,7 +118,22 @@ function signUp () {
     input.classList.add(name)
     input.name = name
     let label = document.createElement('label')
-    label.textContent = `${name}`
+
+    switch (name) {
+      case 'FirstName':
+        label.textContent = `First name`
+        break
+
+      case 'LastName':
+        label.textContent = `Last name`
+        break
+
+      default:
+        label.textContent = `${name}`
+        break
+    }
+
+    // label.textContent = `${name} hej`
     label.setAttribute('for', name)
     if (name == 'Password') {
       input.type = 'password'
@@ -104,12 +142,13 @@ function signUp () {
     inputDiv.append(label)
     inputDiv.append(input)
     signUpDiv.append(inputDiv)
+    
   }
 
   logInDiv.append('If you have an account -')
-  logInText.textContent = 'Log in'
+  logInText.textContent = 'Log In'
   logInText.addEventListener('click', logIn)
-  logInText.style.color = 'teal'
+  logInText.style.color = '#1e608f'
   logInText.style.cursor = 'pointer'
 
   logInDiv.append(logInText)
